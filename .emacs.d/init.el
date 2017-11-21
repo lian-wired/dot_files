22e5a137d503fbebbe5fdcca2a33876d2591a578
lian <lian.wired@gmail.com>
2017-11-21 11:37:30 +0900

    Add wgrep-ag.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,7 +22,7 @@
     ("283c87234b7df63a8b0ec7653a1693439e9f8e6942bd32c8b277ed66b06af074" default)))
  '(package-selected-packages
    (quote
-    (which-key anything visual-regexp-steroids visual-regexp yasnippet flycheck flycheck-css-colorguard flycheck-julia crosshairs auto-install revive real-auto-save package-utils init-loader zencoding-mode yaml-mode viewer undo-tree smartrep slime-repl sass-mode ruby-electric ruby-block rhtml-mode redo+ paredit org migemo markdown-mode magit inf-ruby git-commit-mode flymake-sass flymake-ruby flymake-jslint flymake-haml flymake-css flymake feature-mode evernote-mode csv-mode color-theme coffee-mode auto-complete)))
+    (wgrep-ag which-key anything visual-regexp-steroids visual-regexp yasnippet flycheck flycheck-css-colorguard flycheck-julia crosshairs auto-install revive real-auto-save package-utils init-loader zencoding-mode yaml-mode viewer undo-tree smartrep slime-repl sass-mode ruby-electric ruby-block rhtml-mode redo+ paredit org migemo markdown-mode magit inf-ruby git-commit-mode flymake-sass flymake-ruby flymake-jslint flymake-haml flymake-css flymake feature-mode evernote-mode csv-mode color-theme coffee-mode auto-complete)))
  '(tab-width 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
